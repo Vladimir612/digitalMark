e7cf6c0cb62b759e8880551931651dd77c42fcb1
djordjemojsic <djordjemojsic2001@gmail.com>
2022-03-16 20:46:26 +0100

    nesto233

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Dashboard from './pages/Dashboard/Dashboard'
-import Login from './pages/Login/Login'
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Routes>
-        <Route path='/' element={<Navigate to='/login' />} />
-        <Route path='/login' element={<Login />} />
-
-        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <p></p>
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
