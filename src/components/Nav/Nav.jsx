2e574f7e2399eb586704dd49b815cd3b66ef2608
Vladimir Lazarevic <vladimir.12.lazarevic@gmail.com>
2022-03-16 15:19:56 +0100

    Radjeno na navu

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,23 +1,65 @@
 import React from "react";
 import "./Nav.scss";
+import {
+  BsClipboardCheck,
+  BsClipboardX,
+  BsClipboardPlus,
+  BsPeopleFill,
+  BsStarFill,
+  BsPeople,
+  BsClipboardData,
+  BsPersonFill,
+} from "react-icons/bs";
+import { AiOutlineFileDone } from "react-icons/ai";
+import { GiHamburgerMenu } from "react-icons/gi";
+import logo from "../../assets/images/logo.png";
 
 const Nav = () => {
   return (
     <nav>
-      <div className="toggle-nav"></div>
+      <div className="toggle-nav">
+        <GiHamburgerMenu />
+      </div>
+      <div className="logo">
+        <img src={logo} alt="C2S" />
+      </div>
       <ul className="nav-tabovi">
-        <li>Neocenjene prijave</li>
-        <li>Ocenjene prijave</li>
-        <li>Finalno</li>
-        <li>Označene</li>
-        <li>Moje izmene</li>
-        <li>Nisu smešteni</li>
-        <li>Smešteni</li>
-        <li>Trenutno stanje</li>
+        <li>
+          <BsClipboardX />
+          <span>Neocenjene prijave</span>
+        </li>
+        <li>
+          <BsClipboardCheck />
+          <span>Ocenjene prijave</span>
+        </li>
+        <li>
+          <AiOutlineFileDone />
+          <span>Finalno</span>
+        </li>
+        <li>
+          <BsStarFill />
+          <span>Označene</span>
+        </li>
+        <li>
+          <BsClipboardPlus />
+          <span>Moje izmene</span>
+        </li>
+        <li>
+          <BsPeople />
+          <span>Nisu smešteni</span>
+        </li>
+        <li>
+          <BsPeopleFill />
+          <span>Smešteni</span>
+        </li>
+        <li>
+          <BsClipboardData />
+          <span>Trenutno stanje</span>
+        </li>
       </ul>
       <div className="korisnik">
         <div className="korisnik-slika">
-          <img src="" alt="" />
+          <BsPersonFill />
         </div>
         <p className="ime-korisnika">Pera Perić</p>
       </div>
